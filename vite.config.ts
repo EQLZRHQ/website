356be6b6282e2aafcca2a060d2732c8ea97b48fd
0xherbpowell <herbertwalterpowell@gmail.com>
2025-06-01 22:10:28 +0530

    added dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,26 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  define: {
+    global: 'window',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['react', 'react-dom', 'react-router-dom'],
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      overlay: false,
+    },
+    allowedHosts: [
+      '.eqlzr.xyz',
+    ],
   },
 });
